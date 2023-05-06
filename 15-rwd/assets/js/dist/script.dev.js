755856ed4d3106c75a9bc45e721dc8c100492daf
Olena Rokhmail Beet <erokhmail@gmail.com>
2023-05-06 05:55:12 +0300

    add rwd homework

---
+++ 15-rwd/assets/js/dist/script.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function toggleMenu() {
+  document.getElementById('hamb-bnt').classList.toggle("is-active");
+  document.getElementById('mobile-sidebar').classList.toggle('open');
+  document.getElementById('shadow').classList.toggle('show');
+}
---
+++ 15-rwd/assets/js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.getElementById('hamb-bnt').classList.toggle("is-active");
+    document.getElementById('mobile-sidebar').classList.toggle('open')
+    document.getElementById('shadow').classList.toggle('show');
+}
