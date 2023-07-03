b19ec79060d18a92d96b2ac106edf21684420eb9
Olena Rokhmail Beet <erokhmail@gmail.com>
2023-07-03 18:56:13 +0300

    upd second exam

--- second-exam-js/assets/js/script.js
+++ second-exam-js/assets/js/script.js
@@ -12,7 +12,7 @@ const lazyLoadInstance = new LazyLoad({});
 
     
   $(document).ready(function(){
- /*big Slider */
+ /*/*-====================big Slider ====================-*/
     $('.big-slider').slick({
       arrows: false,
       dots: true,
@@ -30,34 +30,10 @@ const lazyLoadInstance = new LazyLoad({});
         }
       ]
     });
- /*#big Slider */
-  /*NEWS Slider */
-  // $('.news-slider').slick({
-  //   arrows: true,
-  //   prevArrow: '<button class="news-prev"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="19.5" stroke="white"/><path d="M21.8984 17.25C21.8984 17.1797 21.8672 17.1172 21.8203 17.0703L21.4297 16.6797C21.3828 16.6328 21.3125 16.6016 21.25 16.6016C21.1875 16.6016 21.1172 16.6328 21.0703 16.6797L17.4297 20.3203C17.3828 20.3672 17.3516 20.4375 17.3516 20.5C17.3516 20.5625 17.3828 20.6328 17.4297 20.6797L21.0703 24.3203C21.1172 24.3672 21.1875 24.3984 21.25 24.3984C21.3125 24.3984 21.3828 24.3672 21.4297 24.3203L21.8203 23.9297C21.8672 23.8828 21.8984 23.8125 21.8984 23.75C21.8984 23.6875 21.8672 23.6172 21.8203 23.5703L18.75 20.5L21.8203 17.4297C21.8672 17.3828 21.8984 17.3125 21.8984 17.25Z" fill="white"/></svg></button>',
-  //   nextArrow: '<button class="news-next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="19.5" stroke="white"/><path d="M22.6484 20.5C22.6484 20.4375 22.6172 20.3672 22.5703 20.3203L18.9297 16.6797C18.8828 16.6328 18.8125 16.6016 18.75 16.6016C18.6875 16.6016 18.6172 16.6328 18.5703 16.6797L18.1797 17.0703C18.1328 17.1172 18.1016 17.1875 18.1016 17.25C18.1016 17.3125 18.1328 17.3828 18.1797 17.4297L21.25 20.5L18.1797 23.5703C18.1328 23.6172 18.1016 23.6875 18.1016 23.75C18.1016 23.8203 18.1328 23.8828 18.1797 23.9297L18.5703 24.3203C18.6172 24.3672 18.6875 24.3984 18.75 24.3984C18.8125 24.3984 18.8828 24.3672 18.9297 24.3203L22.5703 20.6797C22.6172 20.6328 22.6484 20.5625 22.6484 20.5Z" fill="white"/></svg></button>',
-  //   dots: true,
-  //   speed: 1500,
-  //   slidesToShow: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 2,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 991,
-  //       settings: {
-  //         slidesToShow: 1,
-  //       }
-  //     },
-      
-  //   ]
-  // });
-/*#NEWS Slider */
+ /*/*-====================#big Slider====================- */
 
-/*next section */
+
+/*/*-====================next section====================- */
 
     $(".scrolldown").click(function(event) {
       event.preventDefault();
@@ -136,7 +112,7 @@ const lazyLoadInstance = new LazyLoad({});
   getNews();
 
 
-  /*LIGHT Gallery */
+  /*-====================LIGHT Gallery ====================-*/
   lightGallery(document.getElementById('gallery'), {
     plugins: [lgThumbnail, lgZoom],
     licenseKey: '4342-2322-2344-4434',
@@ -147,9 +123,14 @@ const lazyLoadInstance = new LazyLoad({});
 });
   /*LIGHT Gallery */
 
-  /*Contacts - MAP */
+  /*-====================Contacts - MAP====================- */
+  window.map_active.onclick = function() {
+    window.map_active.innerHTML = '';
+    initMap();
+  };
+
   function initMap(){
-    const map = L.map('contact-map').setView([40.7800531, -73.9634008], 13);
+    const map = L.map('contact-map', { scrollWheelZoom: false }).setView([40.7800531, -73.9634008], 13);
     var circleIcon = L.icon({
       iconUrl: 'assets/images/map-pin.png',
       iconSize:     [106, 106], // size of the icon
@@ -163,4 +144,4 @@ const lazyLoadInstance = new LazyLoad({});
       L.marker([40.7800531, -73.9634008], {icon: circleIcon}).addTo(map)
           .bindPopup('<div class="details">The Metropolitan</div>')
   }
-  /*Contacts - MAP */
+  /*-====================Contacts - MAP ====================-*/
